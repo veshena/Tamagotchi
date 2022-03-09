d2ef6fc8301a2b9c975a0122f8e7fbb56ace68f9
VESHENA WILLIAMS <98852629+veshena@users.noreply.github.com>
2022-03-09 12:14:37 -0500

    Created my class and instantiate it

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("Veshena")
+// console.log("Veshena")
+
+// Make a class for my tamagotchi game then use the constructor method to instantiate it. Then put in (name) after constructor becausee that's all I want the user to control.... Use ' this. ' to display the : hunger, sleepiness, boredom and age, and start everything at 1 because it's a 1-10 scale.
+class Sheen {
+    constructor(name) {
+        this.name = name
+        this.hunger = 1
+        this.sleepiness = 1
+        this.boredom = 1
+        this.age = 1
+    }
+}
+// Double check if it's working so I would be able put anything else in my new sheen.
+let sheen1 = new Sheen("sheen1")
+// Checking
+console.log(sheen1)
