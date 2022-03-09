b22d8a0f2f5ab366c46b88cb1357c55573cb5632
VESHENA WILLIAMS <98852629+veshena@users.noreply.github.com>
2022-03-09 15:24:27 -0500

    Moved my script tag and added an event listener

--- js/main.js
+++ js/main.js
@@ -10,7 +10,33 @@ class Sheen {
         this.age = 1
     }
 }
-// Double check if it's working so I would be able put anything else in my new sheen.
-let sheen1 = new Sheen("sheen1")
-// Checking
-console.log(sheen1)
+
+// Select buttons using document.querySelector to select the buttons I need to function
+const startButton = document.querySelector("#start")
+console.log(startButton)
+const restartButton = document.querySelector("#restart")
+console.log(restartButton)
+const feedButton = document.querySelector("#feed")
+const playButton = document.querySelector("#play")
+const sleepButton = document.querySelector("#sleep")
+
+// Time to set up the game by making a game object and call it game, use strings so it remains empty that way for every game they must be able to insert a name.
+const game = {
+    sheenName: "",
+
+// instantiate my tamagotchi imma name it sheen and set it with an empty string until im finish creating the object then the value of tamagotchi will be where the string is.
+    sheen: "",
+
+// Im using prompt by making 2 methods. One to set the name and one to start the game. In set name that's where I'll be asking the user name, create the game and update the name to display whatever the user puts
+    setName() {
+    this.sheenName = prompt("WASSUPPPPPPP!!!", "Add Your Name")
+    },
+    startGame() {
+    this.setName()
+    }
+}
+
+// Add event listener for my game
+    startButton.addEventListener("click", (event) => {
+        game.startGame()
+})
