b08c7be39fde06ba0c30990c9db08bf45625e99a
VESHENA WILLIAMS <98852629+veshena@users.noreply.github.com>
2022-03-11 11:33:37 -0500

    Tried making a restart button but it isnt working

--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@ class Sheen {
         this.hunger = 1
         this.sleepiness = 1
         this.boredom = 1
-        this.age = 1
+        this.age = 0
+        this.intHunger = null
+
     }
     // Getting the buttons to work so when i click it the specific events should go down. Make 3 mehtods feedme, sleepytime amd yayplay  and if they cross 10 the pet should die.
     feedMe() {
@@ -56,6 +58,19 @@ const game = {
     // instantiate my tamagotchi imma name it sheen and set it with an empty string until im finish creating the object then the value of tamagotchi will be where the string is.
     sheen: "",
 
+    statusCheck() {
+        if (this.sheen.hunger >= 10 || this.sheen.sleepiness >= 10 || this.sheen.boredom >= 10) {
+            // console.log(sheenAging)
+            // this.sheen.intHunger = clearInterval()
+            this.sheenPlay = clearInterval()
+            clearInterval(this.sheenAging)
+            clearInterval(this.sheenHungry)
+            clearInterval(this.sheenPlay)
+            clearInterval(this.sheenSleepy)
+        }
+    },
+
+
     // Im using prompt by making 2 methods. One to set the name and one to start the game. In set name that's where I'll be asking the user name, create the game and update the name to display whatever the user puts
     setName() {
         this.sheenName = prompt("WASSUPPPPPPP!!!", "Add Your Name")
@@ -77,68 +92,80 @@ const game = {
         this.sheenPlay()
         this.sheenHungry()
     },
-    
-    
-
-// Make the gameover to kick in once i hit 10 and add the restart function. Set the timer to every 3 seconds and also set it to change on display
-sheenAging() {
-    const intervalID = setInterval(() => {
-    console.log("I'm getting old")
-    this.sheen.grow()
-    }, 2000)},
-
-sheenSleepy(){
-    const intervalID = setInterval(() => {
-        console.log("I'm Hungry")
-        this.sheen.sleep()
-    }, 4000)
-},
-    
-sheenHungry() {
-    const intervalID = setInterval(() => {
-        console.log("I'm Hungry")
-        this.sheen.hungry()
-        this.updateDisplay()
-    }, 3000)
-},
-    
-sheenPlay() {
-    const intervalID = setInterval(() => {
-        console.log("I wanna play")
-        this.sheen.play()
-    }, 4000)
-},
-updateDisplay() {
-feedDisplay.innerText = (`Hungry: ${this.sheen.hunger}`)
-sleepDisplay.innerText = (`Sleepy: ${this.sheen.sleepiness}`)
-ageDisplay.innerText = (`Age: ${this.sheen.age}`)
-playDisplay.innerText = (`Play: ${this.sheen.boredom}`)
-},
 
 
-// To end the game im seeting it where any on my metrics become greater than 10 the game should end. And i set the age to 50 First i use a if statement
-gameOver() {
-    const intervalID = setInterval(() => {
-        console.log("game over")
-        if (this.sheen.hunger >= 10) {
-            document.getElementById("walkin-pic").src = "./images/oh-no.gif"
-            clearInterval(intervalID)
-        }
-        else if (this.sheen.sleepiness >= 10) {
-            document.getElementById("walkin-pic").src = "./images/oh-no.gif"
-            clearInterval(intervalID)
-        } 
-        else if (this.sheen.age >= 50) {
-            document.getElementById("walkin-pic").src = "./images/oh-no.gif"
-            alert(`${this.sheen.name} lived a very happy life 😇 ...`)
-            clearInterval(intervalID)
-        }
-        else if (this.sheen.boredom >= 10) {
-            document.getElementById("walkin-pic").src = "./images/oh-no.gif width ="
-            clearInterval(intervalID)
-        } 
-    }, 1000)
-}
+
+    // Make the gameover to kick in once i hit 10 and add the restart function. Set the timer to every 3 seconds and also set it to change on display
+    sheenAging() {
+        const intervalID = setInterval(() => {
+            // console.log("I'm getting old")
+            this.sheen.grow()
+        }, 2000)
+    },
+
+    sheenSleepy() {
+        const intervalID = setInterval(() => {
+            // console.log("I'm Hungry")
+            if (this.sheen.sleepiness < 10) {
+                this.statusCheck()
+                this.sheen.sleep()
+            }
+            else if (this.sheen.sleepiness >= 10) {
+                this.statusCheck()
+            }
+        }, 4000)
+    },
+
+    sheenHungry() {
+        const intervalID = this.intHunger = setInterval(() => {
+            // console.log("I'm Hungry")
+            this.sheen.hungry()
+            this.updateDisplay()
+        }, 3000)
+    },
+
+    sheenPlay() {
+        const intervalID = setInterval(() => {
+            // console.log("I wanna play")
+            this.sheen.play()
+        }, 4000)
+    },
+    updateDisplay() {
+        feedDisplay.innerText = (`Hungry: ${this.sheen.hunger}`)
+        sleepDisplay.innerText = (`Sleepy: ${this.sheen.sleepiness}`)
+        ageDisplay.innerText = (`Age: ${this.sheen.age}`)
+        playDisplay.innerText = (`Play: ${this.sheen.boredom}`)
+    },
+
+
+    // To end the game im seeting it where any on my metrics become greater than 10 the game should end. And i set the age to 50 First i use a if statement
+    intervalID: null,
+    gameOver() {
+        this.intervalID = setInterval(() => {
+            // console.log("game over")
+            if (this.sheen.hunger >= 10) {
+                document.getElementById("walkin-pic").src = "./images/oh-no.gif"
+                this.stopGame()
+            }
+            else if (this.sheen.sleepiness >= 10) {
+                document.getElementById("walkin-pic").src = "./images/oh-no.gif"
+                clearInterval(intervalID)
+            }
+            else if (this.sheen.age >= 50) {
+                document.getElementById("walkin-pic").src = "./images/oh-no.gif"
+                alert(`${this.sheen.name} lived a very happy life 😇 ...`)
+                clearInterval(intervalID)
+            }
+            else if (this.sheen.boredom >= 10) {
+                document.getElementById("walkin-pic").src = "./images/oh-no.gif"
+                clearInterval(intervalID)
+            }
+        }, 1000)
+    },
+    stopGame() {
+        console.log(this)
+        // clearInterval(this.intervalID())
+    }
 }
 
 
@@ -159,7 +186,7 @@ feedButton.addEventListener("click", (event) => {
     // console.log("click click")
     game.sheen.feedMe()
     document.getElementById("walkin-pic").src = "./images/eating.gif"
-    console.log(game.sheen)
+    // console.log(game.sheen)
 })
 
 // Add event listener to my play buton
@@ -167,7 +194,7 @@ playButton.addEventListener("click", (event) => {
     // console.log("click click")
     game.sheen.yayPlay()
     document.getElementById("walkin-pic").src = "./images/playin.gif"
-    console.log(game.sheen)
+    // console.log(game.sheen)
 })
 
 // Add event listener to my sleep button
@@ -175,7 +202,26 @@ sleepButton.addEventListener("click", (event) => {
     // console.log("click click")
     game.sheen.sleepyTime()
     document.getElementById("walkin-pic").src = "./images/sleepy.gif"
-    console.log(game.sheen)
+    // console.log(game.sheen)
 })
 
-// Restart game
+
+// Restart
+function restartGame() {
+    if (this.sheen.hunger >= 10) {
+// this leaves the function if sheen is at least 10.
+    return;
+
+// this message displays only if sheen is less than 10.
+    alert("I'm Straving");
+    }
+    else if (this.sheen.sleepiness >= 10) {
+    return;
+    alert("I'm Sleepy");
+    }
+    else if (this.sheen.boredom >= 10) {
+    return;
+    alert("I'm Bored");
+    }
+}
+restartGame()
